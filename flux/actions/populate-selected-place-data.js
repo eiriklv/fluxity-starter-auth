0d5b831c9ade7ad363f1b795d8fd7ca4e74d82d2
Eirik Vullum <evullum@gmail.com>
2015-03-08 23:26:39 +0100

    check if collection store has the requested single item and use that before hitting the database/backend

--- flux/actions/populate-selected-place-data.js
+++ flux/actions/populate-selected-place-data.js
@@ -3,6 +3,21 @@
 module.exports = function(context, payload, done=(()=>{})) {
   context.Dispatcher.emit('SET_SELECTED_PLACE_AS_LOADING');
 
+  // check if the place/data requested already exists in
+  // the places store and fetch it from there. If not,
+  // fetch directly from the database
+  let places = context.Stores.Places.getState().data || [];
+
+  let place = places.reduce(function(res, cur) {
+    if (cur._id === payload.params.id) res.push(cur);
+    return res;
+  }, [])[0];
+
+  if (place) {
+    context.Dispatcher.emit('POPULATE_SELECTED_PLACE_DATA', place);
+    return done();
+  }
+
   context.Services.getPlaceDetails({
     params: payload.params,
     query: payload.query,
